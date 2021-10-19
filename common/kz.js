9cac729572d4fecb5fa83e05a6cfae1f810ff17e
koozone <empakoo@empal.com>
2021-10-19 11:26:07 +0900

    common 업로드

---
+++ common/kz.js
@@ -0,0 +1,17 @@
+define([
+	'common/module',
+	'common/util',
+], function(module, util) {
+	var kz;
+
+	return {
+		init: function(kzMc) {
+			kz = kzMc;
+			kz.module = module;
+			kz.util = util;
+
+			kz.module.init(kz);
+			kz.util.init(kz);
+		},
+	}
+});
